fccdc06fea333d32a1bace6db0510dbbb639b290
杨顺翔 <yangsx95@qq.com>
2021-11-29 15:22:52 +0800

    feat: websocket_demo

--- go语言圣经/练习答案/2. 程序结构/2.6 包和文件/练习2.1/tempconv/conv.go
+++
@@ -1,7 +0,0 @@
-package tempconv
-
-// CToF converts a Celsius temperature to Fahrenheit. 摄氏度转换华氏度
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-
-// FToC converts a Fahrenheit temperature to Celsius. 华氏度转换摄氏度
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
--- src/gopl.io/ch7/p7.4/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-
-}
-
-func NewParse(h string) {
-
-}
